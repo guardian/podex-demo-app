9800479c57835556119203a8fc42b00eb75345bc
rohanspencer <robo.spencer@gmail.com>
2020-03-09 11:02:35 +1100

    new event entities

---
+++ core/src/main/java/com/guardian/core/podxevent/PodXCallPromptEvent.kt
@@ -0,0 +1,22 @@
+package com.guardian.core.podxevent
+
+import android.os.Parcelable
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+@Entity(tableName = "podx_call_events",
+    indices = [Index(value = ["feedItemUrlString"], unique = false)])
+@Parcelize
+class PodXCallPromptEvent (
+        val timeStart: Long,
+        val timeEnd: Long,
+        val caption: String,
+        val notification: String,
+        val feedItemUrlString: String,
+        val phoneNumber: String,
+        @PrimaryKey(autoGenerate = true)
+        val id: Int = 0
+) : Parcelable
---
+++ core/src/main/java/com/guardian/core/podxevent/PodXPollEvent.kt
@@ -0,0 +1,24 @@
+package com.guardian.core.podxevent
+
+import android.os.Parcelable
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+@Entity(tableName = "podx_poll_events",
+    indices = [Index(value = ["feedItemUrlString"], unique = false)])
+@Parcelize
+data class PodXPollEvent (
+    val timeStart: Long,
+    val timeEnd: Long,
+    val urlString: String,
+    val caption: String,
+    val notification: String,
+    val feedItemUrlString: String,
+    @Embedded
+    var ogMetadata: OGMetadata,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0
+) : Parcelable
