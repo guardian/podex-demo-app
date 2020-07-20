104b272c5eacc40a7448ed3f77d17aa36f68a459
rohanspencer <robo.spencer@gmail.com>
2020-07-20 17:10:57 +1000

    replace fragments with fragment container views

--- core/src/main/java/com/guardian/core/search/api/spoofedtestfeed/SpoofedTestFeedApiImpl.kt
+++ core/src/main/java/com/guardian/core/search/api/spoofedtestfeed/SpoofedTestFeedApiImpl.kt
@@ -12,7 +12,7 @@ class SpoofedTestFeedApiImpl : SpoofedTestFeedApi {
         ),
         SearchResult(
             "original podx test feed",
-            "https://uploads.guim.co.uk/2020/05/07/PodcastArt_FullStory_(2).jpg",
+            "https://uploads.guim.co.uk/2019/06/03/Australian_Politics_Live_3000x3000.jpg",
             "https://interactive.guim.co.uk/podx/podcast.xml"
         )
     )
